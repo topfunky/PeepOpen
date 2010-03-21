e7aaedda6e87a70488d4110b5095605699b448ce
Geoffrey Grosenbach <boss@topfunky.com>
2010-03-21 00:44:00 -0600

    WIP Handle the enter key

--- Classes/Controllers/FuzzyWindowController.rb
+++ Classes/Controllers/FuzzyWindowController.rb
@@ -5,14 +5,46 @@
 # Copyright 2010 Topfunky Corporation. All rights reserved.
 
 class FuzzyWindowController < NSWindowController
-  
+
   attr_accessor :tableViewController, :window
-  
+
   # TODO: Respond to ENTER in text field
-  
+
   def didSearchForString(sender)
     tableViewController.searchForString(sender.stringValue)
   end
+  
+  # Received when user hits Enter in search field.
+  def controlTextDidEndEditing(aNotification)
+    NSLog "ended editing"
+  end
+
+
+
+  # -(BOOL)control:(NSControl*)control textView:(NSTextView*)textView doCommandBySelector:(SEL)commandSelector {
+  #     BOOL result = NO;
+  #     if (commandSelector == @selector(insertNewline:)) {
+  #         // enter pressed
+  #         result = YES;
+  #     }
+  #     else if(commandSelector == @selector(moveLeft:)) {
+  #         // left arrow pressed
+  #         result = YES;
+  #     }
+  #     else if(commandSelector == @selector(moveRight:)) {
+  #         // rigth arrow pressed
+  #         result = YES;
+  #     }
+  #     else if(commandSelector == @selector(moveUp:)) {
+  #         // up arrow pressed
+  #         result = YES;
+  #     }
+  #     else if(commandSelector == @selector(moveDown:)) {
+  #         // down arrow pressed
+  #         result = YES;
+  #     }
+  #     return result;
+  # }
 
 end
 
